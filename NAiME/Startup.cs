6884ce289534b78ddf86a6a0665c23ef16d6b5fb
Nate Thompson <nthompson@weg.onmicrosoft.com>
2018-05-01 19:39:00 -0400

    Add project files.

---
+++ NAiME/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NAiME.Startup))]
+namespace NAiME
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
