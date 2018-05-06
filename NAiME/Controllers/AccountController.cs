2c4420b8a591c632be2269d4df25552cb91f0561
Nathan Thompson <NateThompson114@gmail.com>
2018-05-06 19:18:58 -0400

    Fixed account controller.

--- NAiME/Controllers/AccountController.cs
+++ NAiME/Controllers/AccountController.cs
@@ -159,14 +159,14 @@ namespace NAiME.Controllers
                 await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
 
                 // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                // Send an email with this link
-                // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                //// Send an email with this link
+                //var code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                 var userInDb = _context.Users.FirstOrDefault(m => m.Id == user.Id);
                 if (userInDb != null)
-                    userInDb.UserToken = userInDb.Id.HashThis(SHA256.Create());
+                    userInDb.UserToken = userInDb.Email.HashThis(SHA256.Create());
 
                 _context.SaveChanges();
 
