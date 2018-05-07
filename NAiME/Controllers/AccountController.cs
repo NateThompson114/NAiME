5fe4fecc9757055e64351fa8141439f28f93b4ea
Nathan Thompson <NateThompson114@gmail.com>
2018-05-06 20:04:48 -0400

    Turned off e-mailier till its ready.

--- NAiME/Controllers/AccountController.cs
+++ NAiME/Controllers/AccountController.cs
@@ -159,7 +159,7 @@ namespace NAiME.Controllers
                 await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
 
                 // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                //// Send an email with this link
+                // Send an email with this link
                 //var code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                 //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                 //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
@@ -192,7 +192,7 @@ namespace NAiME.Controllers
         }
 
         //
-        // GET: /Account/ForgotPassword
+        // GET: /Account/ForgotPassword 7
         [AllowAnonymous]
         public ActionResult ForgotPassword()
         {
