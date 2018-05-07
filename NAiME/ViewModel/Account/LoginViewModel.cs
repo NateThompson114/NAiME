fc43ca7946dcb2f6c976285d3c80cf4349d4bcd1
Nathan Thompson <NateThompson114@gmail.com>
2018-05-06 21:22:25 -0400

    Moved login to username.

--- NAiME/ViewModel/Account/LoginViewModel.cs
+++ NAiME/ViewModel/Account/LoginViewModel.cs
@@ -4,8 +4,11 @@ namespace NAiME.ViewModel.Account
 {
     public class LoginViewModel
     {
-        [Required, Display(Name = "Email"), EmailAddress]
-        public string Email { get; set; }
+        [Required]
+        public string UserName { get; set; }
+
+        //[Required, Display(Name = "Email"), EmailAddress]
+        //public string Email { get; set; }
 
         [Required, DataType(DataType.Password), Display(Name = "Password")]
         public string Password { get; set; }
