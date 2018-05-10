587eca6ddfecb6f1f9edadad605825a0bb88f867
Nathan Thompson <NateThompson114@gmail.com>
2018-05-09 21:15:11 -0400

    Added user management and admin role to system.

---
+++ NAiME/Controllers/MembersController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using NAiME.Models;
+using NAiME.ViewModel;
+using NAiME.ViewModel.Account;
+
+namespace NAiME.Controllers
+{
+    [Authorize(Roles = RoleName.Admin)]
+    public class MembersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MembersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Members
+        public ActionResult Index()
+        {
+            var usersWithRoles = (from user in _context.Users
+                select new
+                {
+                    UserId = user.Id,
+                    Username = user.UserName,
+                    user.Email,
+                    RoleNames = (from userRole in user.Roles
+                        join role in _context.Roles on userRole.RoleId
+                            equals role.Id
+                        select role.Name).ToList()
+                }).ToList().Select(p => new UsersInRoleViewModel()
+                {
+                    UserId = p.UserId,
+                    UserName = p.Username,
+                    Email = p.Email,
+                    Role = string.Join(",", p.RoleNames)
+                });
+
+
+            return View(usersWithRoles);
+        }
+
+        public ActionResult New()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+
+            const string roleName = "Admin";
+
+            if (!roleManager.RoleExists(roleName))
+            {
+                var role = new IdentityRole {Name = roleName};
+                roleManager.Create(role);
+            }
+
+            userManager.AddToRole(User.Identity.GetUserId(), roleName);
+
+
+            if (User.IsInRole(roleName))
+            {
+                return Content($"{User.Identity.Name} is in the role of {roleName}");
+            }
+
+            return Content("Failed");
+        }
+    }
+}
